570cd027847777b0e7c6aee4035caf28b4c8c736
Andrii Panchenko <andrevpanchenko@gmail.com>
2023-03-17 21:30:21 +0200

    make responsive

--- src/components/Characters/Characters.tsx
+++ src/components/Characters/Characters.tsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Card from '../Card/Card';
 import './Characters.scss';
 import logo from '../../assets/Rick&Morty.png';
+import searchIcon from '../../assets/search-icon.svg';
 
 export default function Characters() {
+    const inputRef = useRef<HTMLInputElement>(null);;
     const [maxCards, setMaxCards] = useState(Number(localStorage.getItem('maxCards')) || 20);
     const [inputValue, setInputValue] = useState('');
     const [list, setList] = useState<any>([]);
@@ -69,6 +71,12 @@ export default function Characters() {
     const filteredCharacters = list.filter((character: any) =>
         character.name.toLowerCase().includes(inputValue.toLowerCase())
     );
+    const handleContainerClick = () => {
+        inputRef.current!.focus();
+    };
+    const handleImageClick = () => {
+        inputRef.current!.click();
+    };
     
     const listCards = isLoading ? <div>Loading...</div> : filteredCharacters.length === 0 || filteredCharacters === undefined ? <div>Empty</div> :
     filteredCharacters?.slice(0, maxCards).map((item: any, idx: number) => {
@@ -77,9 +85,9 @@ export default function Characters() {
     return (
         <div className='wrapper'>
             <img className='logo' src={logo} alt="Rick & Morty" />
-            <div className='filter'>
-                <img src="" alt="" />
-                <input type="text" value={inputValue} onChange={handleInputChange} placeholder="Filter by name..." />
+            <div className='filter' onClick={handleContainerClick}>
+                <img className='search-icon' src={searchIcon} alt="icon" onClick={handleImageClick} />
+                <input className='filter-input' type="text" value={inputValue} onChange={handleInputChange} ref={inputRef} placeholder="Filter by name..." />
             </div>
             <div className='characters-wrapper'>
                 {listCards}
